aa26f0398df9298a98179cb290081c4838f9ce32
DFreyerPereira <jfreyermartins@gmail.com>
2026-01-13 16:07:27 +0100

    changes to db create table file

--- DBCreation.sql
+++ DBCreation.sql
@@ -1,22 +1,35 @@
 -- database creation
 
-CREATE TABLE dim_persons (
-    personID int not null,
-    deptID varchar(255),
-    schoolID varchar(255),
-    uniID varchar(255),
-    addressID varchar(255),
-    constraint persons_pk PRIMARY KEY (personID)
+CREATE TABLE dim_customers (
+    id INTEGER PRIMARY KEY UNIQUE,
+    customerID INTEGER NOT NULL,
+    customerName VARCHAR(255) NOT NULL,
+    customerAge VARCHAR(255),
+    customerCity VARCHAR(255),
+    createDate TIMESTAMP NOT NULL
 );
 
-create table dim_scores (
-	scoreID int NOT NULL,
-	scoreName varchar(255) NOT NULL,
-	constraint scores_pk primary	KEY (scoreID);
+create table dim_items (
+	id INTEGER PRIMARY KEY UNIQUE,
+	itemID INTEGER NOT NULL,
+	itemName VARCHAR(255) NOT NULL,
+	currentItemPrice FLOAT NOT NULL,
+	createDate TIMESTAMP NOT NULL,
+	updateDate TIMESTAMP NOT NULL
+);
 
-create table fct_scores (
-	eventID int NOT NULL,
-	personID int NOT NULL,
-	scoreID int NOT NULL,
-	score int NOT NULL 
+CREATE TABLE dim_locations (
+	id INTEGER PRIMARY KEY UNIQUE,
+	locationID INTEGER,
+	locationName VARCHAR(255),
+	locationStreet VARCHAR(255),
+	locationPostCode VARCHAR(255)
+);
+
+create table fct_purchases (
+	eventID INTEGER PRIMARY KEY UNIQUE,
+	customerID INTEGER NOT NULL,
+	itemID INTEGER NOT NULL,
+	locationID INTEGER NOT NULL,
+	purchaseTime TIMESTAMP NOT NULL
 );
