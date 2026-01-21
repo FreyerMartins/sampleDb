96d50bbde6be2b3243cd5989c95aace1484d5613
DFreyerPereira <jfreyermartins@gmail.com>
2026-01-21 14:14:42 +0100

    generated the person data generation file

---
+++ main.py
@@ -0,0 +1,22 @@
+import faker
+fake = faker.Faker()
+
+seed = 134
+
+def generate_person(seed):
+    person = {
+        'name': fake.name(),
+        'city': fake.city(),
+        'state': fake.state(),
+        'zip_code': fake.zipcode(),
+        'country': fake.country(),
+        'email': fake.email(),
+        'job': fake.job(),
+        'company': fake.company(),
+        'ssn': fake.ssn(),
+        'birthdate': fake.date_of_birth(),
+        'phone_number': fake.phone_number()
+    }
+    return person
+
+print(generate_person(seed))
